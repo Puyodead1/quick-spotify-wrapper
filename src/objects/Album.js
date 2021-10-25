c02eef6831ff649dc05913547855126e32b238ed
Puyodead1 <23562356riley@gmail.com>
2021-10-25 15:09:00 -0400

    fix: missing ``external_urls`` property on albums

--- src/objects/Album.js
+++ src/objects/Album.js
@@ -1,18 +1,18 @@
-/* 
+/*
  *  Simple Javascript wrapper for the Spotify API
- *  
+ *
  *  Copyright (c) 2021 Puyodead1
- *  
+ *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
- *  
+ *
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *  
+ *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -40,6 +40,7 @@ class Album {
         ? this.makeArtists(object.artists)
         : null;
     this.available_markets = object.available_markets;
+    this.external_urls = object.external_urls;
     this.copyrights =
       object.copyrights && Array.isArray(object.copyrights)
         ? this.makeCopyrights(object.copyrights)
